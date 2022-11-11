6f398e834fe9cb3c9a9207f435d8c83cd83672ed
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-11 15:27:50 +0500

    added data and functions

---
+++ main.py
@@ -0,0 +1,71 @@
+def json_file_to_dict(file_path: str) -> dict:
+    '''convert json to dict
+    
+    Args:
+        file_path (str): file path.
+        
+    Returns:
+        dict: dict data
+    '''
+    return
+
+
+def get_number_of_users() -> int:
+    '''Find users's count.
+    
+    Args:
+        None
+    
+    Returns:
+        int: number of all users.
+    '''
+    return
+
+
+def get_user_by_id(id: int) -> dict:
+    '''get user by id
+    
+    Args:
+        id (int): user id.
+        
+    Returns:
+        dict: user data
+    '''
+    return
+
+
+def get_user_by_firstname(first_name: str) -> dict:
+    '''get user by first name
+    
+    Args:
+        first_name (str): user's first name.
+        
+    Returns:
+        dict: user data
+    '''
+    return
+
+
+def get_user_by_lastname(last_name: str) -> dict:
+    '''get user by last name
+    
+    Args:
+        first_name (str): user's last name.
+        
+    Returns:
+        dict: user data
+    '''
+    return
+
+
+def get_user_by_country(country: str) -> dict:
+    '''get user by country
+    
+    Args:
+        country (str): name of user's country.
+        
+    Returns:
+        dict: user data
+    '''
+    return
+
